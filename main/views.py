9bf3cfd92ce884e75bda31bff0eee50771cef980
rapunzzz <kurniathaariq55@gmail.com>
2023-09-13 15:42:17 +0700

    Tutorial 2 selesai

--- main/views.py
+++ main/views.py
@@ -37,7 +37,6 @@ def show_json(request):
 def show_xml_by_id(request, id):
     data = Product.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
-
 def show_json_by_id(request, id):
     data = Product.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
