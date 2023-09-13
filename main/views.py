68ba41e905d9f78645c51a4b1c9679e9e4848f2a
rapunzzz <kurniathaariq55@gmail.com>
2023-09-13 15:26:16 +0700

    Tutorial 2 selesai

--- main/views.py
+++ main/views.py
@@ -14,8 +14,8 @@ def show_main(request):
         'class': 'PBP A',
         'products' : products
     }
-
     return render(request, "main.html", context)
+
 def create_product(request):
     form = ProductForm(request.POST or None)
 
@@ -29,12 +29,15 @@ def create_product(request):
 def show_xml(request):
     data = Product.objects.all()
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
 def show_json(request):
     data = Product.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
 def show_xml_by_id(request, id):
     data = Product.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
 def show_json_by_id(request, id):
     data = Product.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -78,13 +78,17 @@ WSGI_APPLICATION = 'shopping_list.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 if PRODUCTION:
     DATABASES = {
-        'default' : env.db('DATABASE_URL')
-} 
+        'default': env.db('DATABASE_URL')
+    }
     DATABASES["default"]["ATOMIC_REQUESTS"] = True
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
