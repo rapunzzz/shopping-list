d51c24945acfa9561aae897f6bbd8d55e2647ab4
rapunzzz <kurniathaariq55@gmail.com>
2023-11-14 21:35:23 +0700

    edit dikit

--- main/views.py
+++ main/views.py
@@ -22,7 +22,7 @@ def show_main(request):
     products = Product.objects.filter(user=request.user)
     context = {
         'name': request.user.username,
-        'class': 'PBP F',
+        'class': 'F',
         'products' : products,
         'last_login': request.COOKIES['last_login'],
     }
